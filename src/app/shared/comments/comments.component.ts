d471be10db8dca5541274cf0380c6cde265589a5
Nick Ater <nickolas.ater@gmail.com>
2020-10-11 20:56:44 -0400

    added ci/cd frameworking

--- src/app/shared/comments/comments.component.ts
+++ src/app/shared/comments/comments.component.ts
@@ -1,16 +1,20 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  OnInit,
+} from '@angular/core';
+import { Comment } from '@nater20k/brass-exchange-instruments';
 
 @Component({
   selector: 'app-comments',
   templateUrl: './comments.component.html',
   styleUrls: ['./comments.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CommentsComponent implements OnInit {
+  @Input() comments: Comment[];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
