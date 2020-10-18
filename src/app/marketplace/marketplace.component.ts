b657d2728954272f2f6753ee8c8d915e01942042
nick ater <nickolas.ater@gmail.com>
2020-10-18 12:20:59 -0400

    added basic functionality to marketplace component

--- src/app/marketplace/marketplace.component.ts
+++ src/app/marketplace/marketplace.component.ts
@@ -1,14 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { NavigationService } from '../services/navigation/navigation.service';
+import { LOCATIONS } from '../services/navigation/navigation.service';
 @Component({
   templateUrl: './marketplace.component.html',
-  styleUrls: ['./marketplace.component.scss']
+  styleUrls: ['./marketplace.component.scss'],
 })
 export class MarketplaceComponent implements OnInit {
+  constructor(private navService: NavigationService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  navigateToInstruments(): void {
+    this.navService.navigateTo(LOCATIONS.INSTRUMENTS.HOME);
   }
 
+  navigateToServices(): void {
+    this.navService.navigateTo(LOCATIONS.services);
+  }
 }
