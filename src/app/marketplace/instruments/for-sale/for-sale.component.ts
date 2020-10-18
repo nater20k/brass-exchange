fa71c0f56c9b94f28bdfbe109aa91ffd3f5e753a
nick ater <nickolas.ater@gmail.com>
2020-10-18 12:23:18 -0400

    prettier changes

--- src/app/marketplace/instruments/for-sale/for-sale.component.ts
+++ src/app/marketplace/instruments/for-sale/for-sale.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { InstrumentApiService } from 'src/app/services/instruments/instrument-api.service';
+import { LOCATIONS, NavigationService } from 'src/app/services/navigation/navigation.service';
 
 @Component({
   selector: 'app-for-sale',
@@ -8,13 +9,10 @@ import { InstrumentApiService } from 'src/app/services/instruments/instrument-ap
   styleUrls: ['./for-sale.component.scss'],
 })
 export class ForSaleComponent {
-  constructor(
-    private instrumentApi: InstrumentApiService,
-    private router: Router
-  ) {}
+  constructor(private instrumentApi: InstrumentApiService, private navService: NavigationService) {}
   instrumentsForSale = this.instrumentApi.getAllForSaleInstruments();
 
-  navigateToDetail(id: string): Promise<boolean> {
-    return this.router.navigate([`instruments/detail/${id}`]);
+  navigateToDetail(id: string): void {
+    this.navService.navigateTo(LOCATIONS.INSTRUMENTS.DETAIL(id));
   }
 }
