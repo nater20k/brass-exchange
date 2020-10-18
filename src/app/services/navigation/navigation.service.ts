56baa0ea71b3bfabd2ee58f1d435d2a09950b279
nick ater <nickolas.ater@gmail.com>
2020-10-18 12:23:39 -0400

    added navigation service

---
+++ src/app/services/navigation/navigation.service.ts
@@ -0,0 +1,24 @@
+import { Location, LocationStrategy } from '@angular/common';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class NavigationService {
+  constructor(private router: Router) {}
+
+  navigateTo(location: string) {
+    this.router.navigate([location]);
+  }
+}
+
+export const LOCATIONS = {
+  INSTRUMENTS: {
+    HOME: 'instruments',
+    BUY: 'instruments/for-sale',
+    SELL: 'instruments',
+    DETAIL: (id: string) => `instruments/detail/${id}`,
+  },
+  services: 'services',
+};
