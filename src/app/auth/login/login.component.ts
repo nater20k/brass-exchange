05b00164561c7215b60cc068a3c909985448c47f
nick ater <nickolas.ater@gmail.com>
2020-10-18 22:47:59 -0400

    built out login component

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  loginFormGroup: FormGroup;
+  constructor(private fb: FormBuilder, public authService: AuthService) {}
 
   ngOnInit(): void {
+    this.loginFormGroup = this.fb.group({
+      username: this.fb.control(''),
+      password: this.fb.control(''),
+    });
   }
-
 }
