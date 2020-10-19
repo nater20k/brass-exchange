40a5efd0ca826d2eb3a4aecf60d7e51f1669a927
nick ater <nickolas.ater@gmail.com>
2020-10-18 22:43:11 -0400

    added full fuctionality to comment

--- src/app/shared/comments/comments.component.ts
+++ src/app/shared/comments/comments.component.ts
@@ -1,5 +1,7 @@
-import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { Comment } from '@nater20k/brass-exchange-instruments';
+import { AuthService } from 'src/app/auth/auth.service';
 
 @Component({
   selector: 'app-comments',
@@ -9,9 +11,34 @@ import { Comment } from '@nater20k/brass-exchange-instruments';
 })
 export class CommentsComponent implements OnInit {
   @Input() comments: Comment[];
-  constructor() {}
+  @Output() newComment = new EventEmitter<Partial<Comment>>();
+  commentForm: FormGroup;
+  constructor(private fb: FormBuilder, private authService: AuthService) {}
 
   ngOnInit(): void {
-    console.log({ comments: this.comments });
+    this.commentForm = this.buildForm();
+  }
+
+  addComment(): void {
+    this.newComment.emit(this.adaptFormToComment());
+    this.commentForm = this.buildForm();
+  }
+
+  private buildForm(): FormGroup {
+    return this.fb.group({
+      content: this.fb.control(''),
+      username: this.fb.control(''), // TODO
+      createdDate: this.fb.control(''), // TODO
+      likes: this.fb.control(0),
+    });
+  }
+
+  private adaptFormToComment(): Comment {
+    return {
+      content: this.commentForm.get('content').value,
+      username: sessionStorage.getItem('displayName') || 'N/A',
+      createdDate: new Date(),
+      likes: 0,
+    };
   }
 }
