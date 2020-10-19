9cd549bd67a738f3d1e46440c8f7ebbfcc62d3c8
nick ater <nickolas.ater@gmail.com>
2020-10-18 22:41:26 -0400

    added new routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
 import { MarketplaceComponent } from './marketplace/marketplace.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: MarketplaceComponent
+    component: MarketplaceComponent,
   },
-  { path: 'instruments', loadChildren: () => import('./marketplace/instruments/instruments.module').then(m => m.InstrumentsModule) },
-  { path: 'services', loadChildren: () => import('./marketplace/services/services.module').then(m => m.ServicesModule) }
+  { path: 'login', component: LoginComponent },
+  {
+    path: 'instruments',
+    loadChildren: () => import('./marketplace/instruments/instruments.module').then((m) => m.InstrumentsModule),
+  },
+  {
+    path: 'services',
+    loadChildren: () => import('./marketplace/services/services.module').then((m) => m.ServicesModule),
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
