2cc82704dd62f308c2ad40d18591840509fe7a50
nick ater <nickolas.ater@gmail.com>
2020-10-18 22:50:28 -0400

    imported reactiveforomsmodule into shared

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CommentsComponent } from './comments/comments.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [CommentsComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   exports: [CommentsComponent],
 })
 export class SharedModule {}
