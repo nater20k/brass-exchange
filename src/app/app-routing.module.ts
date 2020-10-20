f41b91e66b5e576f987509f5f54586f002c02f65
nick ater <nickolas.ater@gmail.com>
2020-10-19 23:23:44 -0400

    added register and contact us to routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 import { MarketplaceComponent } from './marketplace/marketplace.component';
 
 const routes: Routes = [
@@ -9,6 +11,7 @@ const routes: Routes = [
     component: MarketplaceComponent,
   },
   { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
   {
     path: 'instruments',
     loadChildren: () => import('./marketplace/instruments/instruments.module').then((m) => m.InstrumentsModule),
@@ -16,6 +19,10 @@ const routes: Routes = [
   {
     path: 'services',
     loadChildren: () => import('./marketplace/services/services.module').then((m) => m.ServicesModule),
+  },
+  {
+    path: 'contact-us',
+    component: ContactUsComponent,
   },
 ];
 
