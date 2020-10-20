58b6fc8c8a68f27534cc77ed6dc8294250cd7c50
nick ater <nickolas.ater@gmail.com>
2020-10-19 23:27:34 -0400

    added all html to register component

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { UserFormBuilderService, UserFormGroup } from '@nater20k/brass-exchange-users';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
+  registrationFormGroup: UserFormGroup;
+  constructor(private authService: AuthService, private userFormService: UserFormBuilderService) {}
 
   ngOnInit(): void {
+    this.setForm();
+    console.log(this.registrationFormGroup);
   }
 
+  setForm() {
+    this.registrationFormGroup = new UserFormGroup(this.userFormService.registrationForm());
+  }
+
+  submitRegistration() {
+    console.log(this.registrationFormGroup);
+    this.authService.emailRegister(this.registrationFormGroup);
+  }
 }
