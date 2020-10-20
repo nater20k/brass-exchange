39e160a592f98c506b5bc592df71c3df85bbe8c1
nick ater <nickolas.ater@gmail.com>
2020-10-19 23:27:09 -0400

    updated login component

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UserFormBuilderService, UserFormGroup } from '@nater20k/brass-exchange-users';
+import { take } from 'rxjs/operators';
+import { LOCATIONS, NavigationService } from 'src/app/services/navigation/navigation.service';
 import { AuthService } from '../auth.service';
 
 @Component({
@@ -9,12 +12,56 @@ import { AuthService } from '../auth.service';
 })
 export class LoginComponent implements OnInit {
   loginFormGroup: FormGroup;
-  constructor(private fb: FormBuilder, public authService: AuthService) {}
+  loggedOut = false;
+  constructor(
+    private fb: FormBuilder,
+    public authService: AuthService,
+    private navService: NavigationService,
+    private formService: UserFormBuilderService
+  ) {}
 
   ngOnInit(): void {
-    this.loginFormGroup = this.fb.group({
-      username: this.fb.control(''),
-      password: this.fb.control(''),
+    this.initialize();
+  }
+
+  initialize() {
+    this.authService.user$.pipe(take(1)).subscribe((user) => {
+      if (!user) {
+        this.loginFormGroup = this.formService.emailAndPasswordLoginForm();
+        this.loggedOut = true;
+      } else {
+        this.navService.navigateTo(LOCATIONS.INSTRUMENTS.HOME);
+      }
     });
+  }
+
+  emailLogin() {
+    const userLogin = new UserFormGroup(this.loginFormGroup);
+
+    this.authService
+      .emailSignIn(userLogin)
+      .pipe(take(1))
+      .subscribe((user) => {
+        console.log(user);
+        this.navService.navigateTo(LOCATIONS.INSTRUMENTS.HOME);
+      });
+  }
+
+  googleLogin() {
+    this.authService
+      .googleSignIn()
+      .pipe(take(1))
+      .subscribe((user) => {
+        console.log(user);
+        this.navService.navigateTo(LOCATIONS.INSTRUMENTS.HOME);
+      });
+  }
+
+  get email() {
+    return this.loginFormGroup.get('email');
+  }
+
+  get password() {
+    return this.loginFormGroup.get('password');
   }
 }
