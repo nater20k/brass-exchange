57b0bd7b7112cf7b77d46f38e3f1e98b088ed201
nick ater <nickolas.ater@gmail.com>
2020-10-19 23:28:56 -0400

    added more navbar functionality

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { take } from 'rxjs/operators';
+import { AuthService } from 'src/app/auth/auth.service';
+import { LOCATIONS, NavigationService } from 'src/app/services/navigation/navigation.service';
 
 @Component({
   selector: 'app-navbar',
@@ -10,7 +13,28 @@ export class NavbarComponent implements OnInit {
   menuBars = faBars;
   @Input() authenticated = false;
   isMenuOpen = false;
-  constructor() {}
+  constructor(public auth: AuthService, private navService: NavigationService) {}
 
   ngOnInit(): void {}
+  login() {}
+
+  logout() {
+    this.auth
+      .signOut()
+      .pipe(take(1))
+      .subscribe(
+        () => {
+          this.isMenuOpen = false;
+          this.navService.navigateTo(LOCATIONS.LOGIN);
+        },
+        () => {},
+        () => {
+          console.log('complete');
+        }
+      );
+  }
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }
