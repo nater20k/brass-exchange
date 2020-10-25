79735057f191d10f5722fb55427f909cb19c6842
nick ater <nickolas.ater@gmail.com>
2020-10-25 11:20:06 -0400

    updated instrument module

--- src/app/marketplace/instruments/instruments.module.ts
+++ src/app/marketplace/instruments/instruments.module.ts
@@ -3,13 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { InstrumentsRoutingModule } from './instruments-routing.module';
 import { InstrumentsComponent } from './instruments.component';
-import { ForSaleComponent } from './for-sale/for-sale.component';
-import { ForSaleListingComponent } from './for-sale/for-sale-listing/for-sale-listing.component';
+import { ForSaleComponent } from './buy/for-sale.component';
 import { InstrumentDetailComponent } from './instrument-detail/instrument-detail.component';
 import { SharedModule } from '../../shared/shared.module';
+import { SellModule } from './sell/sell.module';
 
 @NgModule({
-  declarations: [InstrumentsComponent, ForSaleComponent, ForSaleListingComponent, InstrumentDetailComponent],
-  imports: [CommonModule, InstrumentsRoutingModule, SharedModule],
+  declarations: [InstrumentsComponent, ForSaleComponent, InstrumentDetailComponent],
+  imports: [CommonModule, InstrumentsRoutingModule, SharedModule, SellModule],
 })
 export class InstrumentsModule {}
