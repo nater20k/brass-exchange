9ae69af80beff4f4f87a0d9bda287189991e8370
nick ater <nickolas.ater@gmail.com>
2020-10-25 11:17:44 -0400

    added forsalelistingcomponent to shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CommentsComponent } from './comments/comments.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { ForSaleListingComponent } from '../marketplace/instruments/buy/for-sale-listing/for-sale-listing.component';
 
 @NgModule({
-  declarations: [CommentsComponent],
+  declarations: [CommentsComponent, ForSaleListingComponent],
   imports: [CommonModule, ReactiveFormsModule],
-  exports: [CommentsComponent],
+  exports: [CommentsComponent, ForSaleListingComponent],
 })
 export class SharedModule {}
