ff11f922acf1abe85d474731046a762afb78620b
nick ater <nickolas.ater@gmail.com>
2020-10-25 21:49:21 -0400

    added input component to shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { CommentsComponent } from './comments/comments.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { ForSaleListingComponent } from '../marketplace/instruments/buy/for-sale-listing/for-sale-listing.component';
+import { InputComponent } from './input/input.component';
 
 @NgModule({
-  declarations: [CommentsComponent, ForSaleListingComponent],
+  declarations: [CommentsComponent, ForSaleListingComponent, InputComponent],
   imports: [CommonModule, ReactiveFormsModule],
-  exports: [CommentsComponent, ForSaleListingComponent],
+  exports: [CommentsComponent, ForSaleListingComponent, InputComponent],
 })
 export class SharedModule {}
