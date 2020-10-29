64994d61fd91ff8390d2b166088f94dd6835d668
nick ater <nickolas.ater@gmail.com>
2020-10-29 09:13:29 -0400

    implemented image component into instrument detail

--- src/app/marketplace/instruments/instrument-detail/instrument-detail.component.ts
+++ src/app/marketplace/instruments/instrument-detail/instrument-detail.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ForSaleListing, Comment } from '@nater20k/brass-exchange-instruments';
 import { Observable } from 'rxjs';
-import { take, tap } from 'rxjs/operators';
+import { take } from 'rxjs/operators';
 import { InstrumentApiService } from 'src/app/services/instruments/instrument-api.service';
 
 @Component({
@@ -13,7 +13,6 @@ import { InstrumentApiService } from 'src/app/services/instruments/instrument-ap
 export class InstrumentDetailComponent implements OnInit {
   titleCasePipe = new TitleCasePipe();
   instrument$: Observable<ForSaleListing>;
-  instrumentId: string;
 
   constructor(private router: Router, private instrumentApi: InstrumentApiService) {}
 
@@ -22,12 +21,10 @@ export class InstrumentDetailComponent implements OnInit {
   }
 
   setInstrument(): void {
-    this.instrument$ = this.instrumentApi
-      .getInstrumentById(this.router.url.split('/')[3])
-      .pipe(tap((instrument) => (this.instrumentId = instrument.id)));
+    this.instrument$ = this.instrumentApi.getInstrumentById(this.router.url.split('/')[3]);
   }
 
-  addComment(comment: Comment) {
-    this.instrumentApi.addCommentToForSaleListing(comment, this.instrumentId).pipe(take(1)).subscribe();
+  addComment(comment: Comment, instrumentId: string) {
+    this.instrumentApi.addCommentToForSaleListing(comment, instrumentId).pipe(take(1)).subscribe();
   }
 }
