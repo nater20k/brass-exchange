84a22ac9febb2f40353c9538b8ab5447d0254026
nick ater <nickolas.ater@gmail.com>
2020-10-29 21:56:01 -0400

    updated image component

--- src/app/shared/image/image.component.ts
+++ src/app/shared/image/image.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ChangeDetectionStrategy, Input } from '@angular/core';
 import { ForSaleListing } from '@nater20k/brass-exchange-instruments';
 import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { finalize, tap } from 'rxjs/operators';
 import { UploadService } from 'src/app/services/upload/upload.service';
 
 @Component({
@@ -15,15 +15,33 @@ export class ImageComponent implements OnInit {
   @Input() isHeroImage = false;
   imageUrls$: Observable<string[]>;
   isLoaded = false;
+  imagesLoaded = false;
+  images: HTMLCollectionOf<HTMLImageElement>;
+
   constructor(private storage: UploadService) {}
 
   ngOnInit(): void {
+    this.images = document.getElementsByTagName<'img'>('img');
     this.imageUrls$ = this.fetchImageUrls({ id: this.instrumentId, sellerEmail: this.sellerEmail }).pipe(
-      tap(() => (this.isLoaded = true))
+      tap(console.log),
+      finalize(() => (this.isLoaded = true))
     );
   }
 
   fetchImageUrls({ id, sellerEmail }: Partial<ForSaleListing>) {
     return this.storage.getImageUrls(`${sellerEmail}/instruments/${id}`);
+  }
+
+  toggleImagesLoaded() {
+    this.setImagesToBlock();
+    this.imagesLoaded = true;
+  }
+
+  private setImagesToBlock() {
+    for (let i = 0; i < this.images.length; i++) this.images[i].style.display = 'inline-block';
+  }
+
+  get isAllPageLoaded() {
+    return this.isLoaded && this.imagesLoaded;
   }
 }
