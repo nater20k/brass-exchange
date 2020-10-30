0af354bc65d8822e942c2de9ca8c1bbf01e225f6
nick ater <nickolas.ater@gmail.com>
2020-10-29 21:57:20 -0400

    added drag scroll library

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,10 +5,12 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { ForSaleListingComponent } from '../marketplace/instruments/buy/for-sale-listing/for-sale-listing.component';
 import { InputComponent } from './input/input.component';
 import { ImageComponent } from './image/image.component';
+import { LoadingComponent } from './loading/loading.component';
+import { DragScrollModule } from 'ngx-drag-scroll';
 
 @NgModule({
-  declarations: [CommentsComponent, ForSaleListingComponent, InputComponent, ImageComponent],
-  imports: [CommonModule, ReactiveFormsModule],
-  exports: [CommentsComponent, ForSaleListingComponent, InputComponent, ImageComponent],
+  declarations: [CommentsComponent, ForSaleListingComponent, InputComponent, ImageComponent, LoadingComponent],
+  imports: [CommonModule, ReactiveFormsModule, DragScrollModule],
+  exports: [CommentsComponent, ForSaleListingComponent, InputComponent, ImageComponent, LoadingComponent],
 })
 export class SharedModule {}
