57c588756173644bda89b56f651e519253660bf0
nick ater <nickolas.ater@gmail.com>
2020-10-31 09:03:44 -0400

    comment component dynamics

--- src/app/shared/comments/comments.component.ts
+++ src/app/shared/comments/comments.component.ts
@@ -1,27 +1,37 @@
 import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Comment } from '@nater20k/brass-exchange-instruments';
+import { switchMap, tap } from 'rxjs/operators';
 import { AuthService } from 'src/app/auth/auth.service';
-
+import { UserApiService } from 'src/app/services/users/user-api.service';
+import { User } from '@nater20k/brass-exchange-users';
 @Component({
   selector: 'app-comments',
   templateUrl: './comments.component.html',
   styleUrls: ['./comments.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CommentsComponent implements OnInit {
   @Input() comments: Comment[];
   @Output() newComment = new EventEmitter<Partial<Comment>>();
   commentForm: FormGroup;
-  constructor(private fb: FormBuilder, private authService: AuthService) {}
+  showComments = false;
+  constructor(private fb: FormBuilder, private authService: AuthService, private userApiService: UserApiService) {}
 
   ngOnInit(): void {
     this.commentForm = this.buildForm();
   }
 
   addComment(): void {
-    this.newComment.emit(this.adaptFormToComment());
-    this.commentForm = this.buildForm();
+    this.authService.user$
+      .pipe(
+        tap(console.log),
+        switchMap((user) => this.userApiService.getSingleUser(user.uid)),
+        tap(console.log)
+      )
+      .subscribe((user: User) => {
+        this.newComment.emit(this.adaptFormToComment(user.displayName));
+        this.commentForm = this.buildForm();
+      });
   }
 
   private buildForm(): FormGroup {
@@ -33,10 +43,11 @@ export class CommentsComponent implements OnInit {
     });
   }
 
-  private adaptFormToComment(): Comment {
+  private adaptFormToComment(username: string): Comment {
+    console.log(username);
     return {
       content: this.commentForm.get('content').value,
-      username: sessionStorage.getItem('displayName') || 'N/A',
+      username,
       createdDate: new Date(),
       likes: 0,
     };
