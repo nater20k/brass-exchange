ed15648b568421234f586406319c617f22b1979e
nick ater <nickolas.ater@gmail.com>
2020-10-31 22:17:10 -0400

    setup new CI/CD for prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCpPkRjc0SzDX7ASRgV7MWxVA-8iRJBAvs',
+    authDomain: 'brass-exchange-prod.firebaseapp.com',
+    databaseURL: 'https://brass-exchange-prod.firebaseio.com',
+    projectId: 'brass-exchange-prod',
+    storageBucket: 'brass-exchange-prod.appspot.com',
+    messagingSenderId: '859269049914',
+    appId: '1:859269049914:web:4cef8ce3d2ab464d270f03',
+    measurementId: 'G-KQGS9DEX7P',
+  },
 };
